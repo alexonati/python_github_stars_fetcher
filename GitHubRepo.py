1c4f5cfb2081fde6f45dfc16d2eecfd8ce1cf6b2
Alex O <53114650+alexonati@users.noreply.github.com>
2022-02-10 13:24:49 +0200

    Delete GitHubRepo.py

--- GitHubRepo.py
+++
@@ -1,9 +0,0 @@
-class GitHubRepo:
-     
-    def __init__(self, name, language, num_stars):
-         self.name = name
-         self.language = language
-         self.num_stars = num_stars
-    
-    def __str__(self):
-        return f'-> {name} is a {language} repo with {stars}'
